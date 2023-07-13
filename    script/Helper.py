ac32cda18f08416a5e2b18e93bc0db62ff5f3947
Emanuel Berger <emanuelberger49@gmail.com>
2023-07-13 16:28:59 +0200

    fix naming of folder

---    script/Helper.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time
-
-# Warten auf Mausklick
-while True:
-    x, y = pyautogui.position()
-    print("Mausklick an Position: x={}, y={}".format(x, y))
-    time.sleep(0.1)
