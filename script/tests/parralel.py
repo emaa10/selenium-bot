1d8a8a97ae656c3a838305dfe2ff81b9334ad32a
Emanuel Berger <emanuelberger49@gmail.com>
2023-07-14 19:31:14 +0200

    updates

--- script/tests/parralel.py
+++ script/tests/parralel.py
@@ -1,42 +1,28 @@
-from multiprocessing import Pool
 from argparse import ArgumentParser
 from tbselenium.tbdriver import TorBrowserDriver
-from tbselenium.utils import launch_tbb_tor_with_stem
-from tbselenium.common import STEM_SOCKS_PORT, USE_RUNNING_TOR,\
-    STEM_CONTROL_PORT
+from time import sleep
 
-JOBS_IN_PARALLEL = 3
-
-
-def run_in_parallel(inputs, worker, no_of_processes=JOBS_IN_PARALLEL):
-    p = Pool(no_of_processes)
-    p.map(worker, inputs)
+# Usage: python bridge.py /path/to/tor-browser-bundle bridge_type
+# bridge_type is one of: obfs3, obfs4, fte, meek-amazon, meek-azure
 
 
-def visit_check_tpo_with_stem(tbb_dir):
+def visit_using_bridge(tbb_dir, bridge_type="meek-amazon"):
     url = "https://check.torproject.org"
-    with TorBrowserDriver(tbb_dir,
-                          socks_port=STEM_SOCKS_PORT,
-                          control_port=STEM_CONTROL_PORT,
-                          tor_cfg=USE_RUNNING_TOR) as driver:
-        driver.load_url(url, wait_on_page=3)
-        print(driver.find_element_by("h1.on").text)
-        print(driver.find_element_by(".content > p").text)
-
-
-def launch_browsers_in_parallel(tbb_path):
-    tor_process = launch_tbb_tor_with_stem(tbb_path=tbb_path)
-    run_in_parallel(JOBS_IN_PARALLEL * [tbb_path],
-                    visit_check_tpo_with_stem)
-    tor_process.kill()
+    with TorBrowserDriver(
+            tbb_dir, default_bridge_type=bridge_type) as driver:
+        driver.load_url(url)
+        print(driver.current_url)  # status text
+        sleep(10)  # To verify that the bridge is indeed uses, go to
+        # Tor Network Settings dialog
 
 
 def main():
-    desc = "Visit check.torproject.org website running 3 browsers in parallel"
+    desc = "Visit check.torproject.org website using a Tor bridge"
     parser = ArgumentParser(description=desc)
     parser.add_argument('tbb_path')
+    parser.add_argument('bridge_type', nargs='?', default="meek-amazon")
     args = parser.parse_args()
-    launch_browsers_in_parallel(args.tbb_path)
+    visit_using_bridge(args.tbb_path, args.bridge_type)
 
 
 if __name__ == '__main__':
