6370a7ec6c1ef7322b7c8be3d314b0407c4ccf7c
Emanuel Berger <emanuelberger49@gmail.com>
2023-07-14 19:25:04 +0200

    comments, counter, random

--- script/mouse-clicks/Bot.py
+++ script/mouse-clicks/Bot.py
@@ -1,31 +1,35 @@
 import time
+import random
 import pyautogui
 
 votes = 10000
 
+i = 0;
+
 # Screen Coords
-x_1 = 1260
+x_1 = 1260 # Jetzt abstimmen
 y_1 = 574
 
-x_2 = 1125
+x_2 = 1125  # Zustimmung, um Inhalte von Friendly Captcha anzuzeigen
 y_2 = 698
 
-x_3 = 1136
+x_3 = 1136  # Anti-Roboter-Verifizierung
 y_3 = 546
 
-x_4 = 1253
+x_4 = 1253  # Stimme abgeben
 y_4 = 539
 
-x_5 = 169
+x_5 = 169   # Tor Menü
 y_5 = 80
 
-x_6 = 273
+x_6 = 273   # Neuer Kanal für diese Seite
 y_6 = 422
 
-x_7 = 1128
+x_7 = 1128  # Lesezeichen aufrufen
 y_7 = 127
 
 for x in range(0, votes):
+    i += 1
     pyautogui.click(x_1, y_1)
     time.sleep(1.5)
 
@@ -47,6 +51,9 @@ for x in range(0, votes):
     pyautogui.click(x_7, y_7)
     time.sleep(5)
 
+    time.sleep(random.randint(1, 8))   # random time to fix detections
+
+    print(str(i) + " mal abgestimmt.")
 
     # pyautogui.hotkey('command', 'shift', '3')
     # time.sleep(0.5)
